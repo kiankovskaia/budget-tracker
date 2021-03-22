57b7df207e946302e52c537095259b6c88be8d97
yourdilemma <k.iankovskaia@gmail.com>
2021-03-22 19:05:48 -0400

    changed db name

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,48 +1,18 @@
-export function checkForIndexedDb() {
-  if (!window.indexedDB) {
-    console.log("Your browser doesn't support a stable version of IndexedDB.");
-    return false;
-  }
-  return true;
+const indexedDb = window.indexedDB;
+
+let db;
+
+const request = indexedDb.open ('budget',1)
+
+request.onupgradeneeded = (event) => {
+let db = event.target.result
+db.createObjectStore('pending',{
+  autoIncrement: true
+}) 
 }
+request.onsuccess = (event) => {
+  db = event.target.result
+  if (navigator.onLine){
 
-export function useIndexedDb(databaseName, storeName, method, object) {
-  return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open(databaseName, 1);
-    let db,
-      tx,
-      store;
-
-    request.onupgradeneeded = function(e) {
-      const db = request.result;
-      db.createObjectStore(storeName, { keyPath: "_id" });
-    };
-
-    request.onerror = function(e) {
-      console.log("There was an error");
-    };
-
-    request.onsuccess = function(e) {
-      db = request.result;
-      tx = db.transaction(storeName, "readwrite");
-      store = tx.objectStore(storeName);
-
-      db.onerror = function(e) {
-        console.log("error");
-      };
-      if (method === "put") {
-        store.put(object);
-      } else if (method === "get") {
-        const all = store.getAll();
-        all.onsuccess = function() {
-          resolve(all.result);
-        };
-      } else if (method === "delete") {
-        store.delete(object._id);
-      }
-      tx.oncomplete = function() {
-        db.close();
-      };
-    };
-  });
+  }
 }
